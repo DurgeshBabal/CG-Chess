0f85772c7c29e0a6fdb67d355c0b6d600f9cfb7e
Amanskywalker <ak47su30ac@gmail.com>
2017-04-09 01:11:33 +0530

    added main of the application  	new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// main.cpp
+
+// headers
+#include <stdio.h>
+
+// graphics headers
+#include <GL/glut.h>
+
+// static headers
+
+// custom class headers
+
+// main of the application
+int myinit()
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
