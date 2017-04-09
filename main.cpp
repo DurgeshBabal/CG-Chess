b7a8703262fd2fed324b5c77a3d00f902baba4b1
Amanskywalker <ak47su30ac@gmail.com>
2017-04-09 11:47:01 +0530

    Added myinit, main and display function  	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,37 @@
 #include <GL/glut.h>
 
 // static headers
+#include <math.h>
 
 // custom class headers
 
-// main of the application
-int myinit()
+void display()
 {
-
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glFlush();
 }
 
+void myinit()
+{
+  glClearColor(1,1,1,1);
+  glColor3f(1,0,0);
+  glPointSize(3);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0,500,0,500);
+}
+
+// main of the application
 int main(int argc, char* argv[])
 {
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("Chess");
+    glutDisplayFunc(display);
+    myinit();
+    glutMainLoop();
     return 0;
 }
