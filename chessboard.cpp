898a5b55d35eec396aefb89a91279ddff2e2c678
Amanskywalker <ak47su30ac@gmail.com>
2017-04-20 23:25:24 +0530

    Added header to generate the Chessboard 	new file:   chessboard.cpp 	new file:   chessboard.h

---
+++ chessboard.cpp
@@ -0,0 +1,51 @@
+// implementation of the class chessboard
+#include "chessboard.h"
+
+// to draw black square
+int ChessBoard::BlackSquare(GLfloat *vertices)
+{
+    //glColor3f(0,0,0);
+    glColor3f((float)153/255, (float)51/255, (float)0/255);
+    Polygon(vertices);
+}
+
+// to draw white square
+int ChessBoard::WhiteSquare(GLfloat *vertices)
+{
+    //glColor3f(1,1,1);
+    glColor3f((float)255/255, (float)230/255, (float)153/255);
+    Polygon(vertices);
+}
+
+int ChessBoard::DrawChessBoard(GLfloat x0, GLfloat y0, GLfloat size)
+{
+    int n = 8; // number of the square in one row
+
+    GLfloat x = x0;
+    GLfloat y = y0;
+
+    for (int i = 0; i < n; ++i)         // row
+    {
+        for (int j = 0; j < n; ++j)         // coloum
+        {
+            GLfloat vertices[] = {x, y, x+size, y, x+size, y+size, x, y+size};
+            if (i%2==0)
+            {
+                if (j%2 == 0)
+                    WhiteSquare(vertices);
+                else
+                    BlackSquare(vertices);
+            }
+            else
+            {
+                if (j%2 == 0)
+                    BlackSquare(vertices);
+                else
+                    WhiteSquare(vertices);
+            }
+            x += size;
+        }
+        x = x0;
+        y += size;
+    }
+}
---
+++ chessboard.h
@@ -0,0 +1,29 @@
+// defines the chess board
+// inherets the shapes class to use basic shapes
+#ifndef CHESSBOARD_H
+#define CHESSBOARD_H
+
+#include <GL/glut.h>
+
+#include "shapes.h"
+
+class ChessBoard :private shapes
+{
+        // to draw black square
+        int BlackSquare(GLfloat *vertices);
+        // to draw white square
+        int WhiteSquare(GLfloat *vertices);
+
+    public:
+        // ChessBoard();
+        //
+        // ~ChessBoard() = default;
+        // ChessBoard(const ChessBoard& other) = default;
+        // ChessBoard(ChessBoard&& other) = default;
+        // ChessBoard& operator=(const ChessBoard& other) = default;
+        // ChessBoard& operator=(ChessBoard&& other) = default;
+
+        int DrawChessBoard(GLfloat x0, GLfloat y0, GLfloat size);
+};
+
+#endif
