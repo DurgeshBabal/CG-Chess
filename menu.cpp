18c9f1d52e4574ca988705535e3e6dbf05c07869
Amanskywalker <ak47su30ac@gmail.com>
2017-04-22 19:59:23 +0530

    Added menu function, binded to mouse right button (tmp for now)  	modified:   menu.cpp  	modified:   menu.h

--- menu.cpp
+++ menu.cpp
@@ -1,10 +1,13 @@
 // source for menu functions
 
-void MainMenu()
+#include "menu.h"
+
+void CreateMainMenu()
 {
     glutCreateMenu(MainMenu);
         glutAddMenuEntry("New Game", 1);
         glutAddMenuEntry("Exit", 2);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
 void MainMenu(int id)
@@ -16,4 +19,5 @@ void MainMenu(int id)
         case 2: exit(0); break; // exit the game
         default: break;
     }
+    glutPostRedisplay();
 }
--- menu.h
+++ menu.h
@@ -3,10 +3,12 @@
 #define MENU_H
 
 #include <GL/glut.h>
+#include <stdlib.h>
+
 // to generate the main menu
-void MainMenu()
+void CreateMainMenu();
 
 // to handle main menu  function calls
-void MainMenu(int id)
+void MainMenu(int id);
 
 #endif
