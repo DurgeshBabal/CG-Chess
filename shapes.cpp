591179894e0bb94aca704925f5d05c864bc5a440
Amanskywalker <ak47su30ac@gmail.com>
2017-04-23 21:39:48 +0530

    Added Functions to draw circle and arch  	modified:   shapes.cpp  	modified:   shapes.h

--- shapes.cpp
+++ shapes.cpp
@@ -1,8 +1,9 @@
 // implementation of the class shapes
 #include "shapes.h"
+#include <math.h>
 
 // vertices = {x1,y1,x2,y2,x3,y3};
-int shapes::Triangle(GLfloat *vertices)
+int BasicShapes::DrawTriangle(GLfloat *vertices)
 {
     // glColor3f(1,1,0);
     // activate and specify pointer to vertex array
@@ -17,7 +18,7 @@ int shapes::Triangle(GLfloat *vertices)
 }
 
 // vertices = {x1,y1,x2,y2,x3,y3,x4,y4};
-int shapes::Polygon(GLfloat *vertices)
+int BasicShapes::DrawQuad(GLfloat *vertices)
 {
     // activate and specify pointer to vertex array
     glEnableClientState(GL_VERTEX_ARRAY);
@@ -28,4 +29,93 @@ int shapes::Polygon(GLfloat *vertices)
     glDisableClientState(GL_VERTEX_ARRAY);
 
     return 1;       // sucessful
+}
+
+// cx, cy = center x, y coordinates
+// r = radius
+// num_segments = number of segments it form to complete the circle (more segment better circle)
+int BasicShapes::DrawCircle(float cx, float cy, float r, int num_segments)
+{
+	float theta = 2 * 3.1415926 / float(num_segments);
+	float c = cosf(theta);//precalculate the sine and cosine
+	float s = sinf(theta);
+	float t;
+
+	float x = r;//we start at angle = 0
+	float y = 0;
+
+	glBegin(GL_LINE_LOOP);
+	for(int ii = 0; ii < num_segments; ii++)
+	{
+		glVertex2f(x + cx, y + cy);//output vertex
+
+		//apply the rotation matrix
+		t = x;
+		x = c * x - s * y;
+		y = s * t + c * y;
+	}
+	glEnd();
+    return 1;
+}
+
+// cx, cy = center x, y coordinates
+// r = radius
+// num_segments = number of segments it form to complete the circle (more segment better circle)
+int BasicShapes::DrawCircleFilled(float cx, float cy, float r, int num_segments)
+{
+	float theta = 2 * 3.1415926 / float(num_segments);
+	float c = cosf(theta);//precalculate the sine and cosine
+	float s = sinf(theta);
+	float t;
+
+	float x = r;//we start at angle = 0
+	float y = 0;
+
+	glBegin(GL_POLYGON);
+	for(int ii = 0; ii < num_segments; ii++)
+	{
+		glVertex2f(x + cx, y + cy);//output vertex
+
+		//apply the rotation matrix
+		t = x;
+		x = c * x - s * y;
+		y = s * t + c * y;
+	}
+	glEnd();
+    return 1;
+}
+
+// cx, cy = center x, y coordinates
+// r = radius
+// start_angle =  starting angle
+// arc_angle = total angle for the arch 
+// num_segments = number of segments it form to complete the circle (more segment better circle)
+int BasicShapes::DrawArc(float cx, float cy, float r, float start_angle, float arc_angle, int num_segments)
+{
+	float theta = arc_angle / float(num_segments - 1);//theta is now calculated from the arc angle instead, the - 1 bit comes from the fact that the arc is open
+
+	float tangetial_factor = tanf(theta);
+
+	float radial_factor = cosf(theta);
+
+
+	float x = r * cosf(start_angle); //we now start at the start angle
+	float y = r * sinf(start_angle);
+
+	glBegin(GL_LINE_STRIP); //since the arc is not a closed curve, this is a strip now
+	for(int ii = 0; ii < num_segments; ii++)
+	{
+		glVertex2f(x + cx, y + cy);
+
+		float tx = -y;
+		float ty = x;
+
+		x += tx * tangetial_factor;
+		y += ty * tangetial_factor;
+
+		x *= radial_factor;
+		y *= radial_factor;
+	}
+	glEnd();
+    return 1;
 }
--- shapes.h
+++ shapes.h
@@ -4,7 +4,7 @@
 
 #include <GL/glut.h>
 
-class shapes
+class BasicShapes
 {
     public:
         // contructors and destructors
@@ -17,10 +17,27 @@ class shapes
 
         // custom function
         // vertices = {x1,y1,x2,y2,x3,y3}
-        int Triangle(GLfloat *vertices);
-        // vertices = {x1,y1,x2,y2,x3,y3,x4,y4}
-        int Polygon(GLfloat *vertices);
+        int DrawTriangle(GLfloat *vertices);
 
+        // vertices = {x1,y1,x2,y2,x3,y3,x4,y4}
+        int DrawQuad(GLfloat *vertices);
+
+        // cx, cy = center x, y coordinates
+        // r = radius
+        // num_segments = number of segments it form to complete the circle (more segment better circle)
+        int DrawCircle(float cx, float cy, float r, int num_segments);
+
+        // cx, cy = center x, y coordinates
+        // r = radius
+        // num_segments = number of segments it form to complete the circle (more segment better circle)
+        int DrawCircleFilled(float cx, float cy, float r, int num_segments);
+
+        // cx, cy = center x, y coordinates
+        // r = radius
+        // start_angle =  starting angle
+        // arc_angle = total angle for the arch
+        // num_segments = number of segments it form to complete the circle (more segment better circle)
+        int DrawArc(float cx, float cy, float r, float start_angle, float arc_angle, int num_segments);
 
 };
 
