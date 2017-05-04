7f8ee45139df1e8cdbea81ab5da8f991da230a7a
Amanskywalker <ak47su30ac@gmail.com>
2017-05-04 19:42:56 +0530

    #name
    # changes
    
    # Bug id :
    
    # change id :
    
    #signoff by:
    #Amanskywalker

--- piece.cpp
+++ piece.cpp
@@ -12,3 +12,28 @@ int Piece::Pawn(GLfloat x, GLfloat y, GLfloat l)
 
     DrawCircleFilled(x+(12.5*l/25),y+(16.5*l/25), 3.5*l/25, 100);
 }
+
+int Piece::Rook(GLfloat x, GLfloat y, GLfloat l)
+{
+
+}
+
+int Piece::Knight(GLfloat x, GLfloat y, GLfloat l)
+{
+
+}
+
+int Piece::Bishop(GLfloat x, GLfloat y, GLfloat l)
+{
+
+}
+
+int Piece::King(GLfloat x, GLfloat y, GLfloat l)
+{
+
+}
+
+int Piece::Queen(GLfloat x, GLfloat y, GLfloat l)
+{
+    
+}
