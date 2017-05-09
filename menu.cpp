47407e4d6ee04f0a8e6ae7c38fbe8adf0df78c72
Amanskywalker <ak47su30ac@gmail.com>
2017-05-09 13:11:25 +0530

    Added menu genration and Display function 	modified:   menu.cpp 	modified:   menu.h

--- menu.cpp
+++ menu.cpp
@@ -2,14 +2,26 @@
 
 #include "menu.h"
 
+Menu::Menu()
+{
+    strcpy(Name, "Menu item");
+    x = 100;
+    y = 100;
+    h = 40;
+    w = 100;
+    FontSize = 14;
+    strcpy(FontFamily, "GLUT_BITMAP_HELVETICA_");
+    ActionId = 0;
+}
+
 int Menu::IsInside(int MouseX, int MouseY)
 {
-    if( ( MouseX > x && MouseX < x+l ) && ( MouseY < y && MouseY > y-l ) )
+    if( ( MouseX > x && MouseX < x+w ) && ( MouseY < y && MouseY > y-h ) )
         return ActionId;
     return 0;
 }
 
-int Menu::AddName(char TempName[], int TempFontSize, char TempFontFamily[])
+int Menu::AddName(char TempName[], int TempFontSize = 14, char TempFontFamily[] = "GLUT_BITMAP_HELVETICA_")
 {
     strcpy(Name, TempName);
     FontSize = TempFontSize;
@@ -19,11 +31,12 @@ int Menu::AddName(char TempName[], int TempFontSize, char TempFontFamily[])
     return 1;
 }
 
-int Menu::AddCoordinates(GLfloat tx, GLfloat ty, GLfloat tl)
+int Menu::AddCoordinates(GLfloat tx, GLfloat ty, GLfloat th = 40, GLfloat tw = 100)
 {
     x = tx;
     y = ty;
-    l = tl;
+    h = th;
+    w = tw;
 
     // operation sucessfull so return true
     return 1;
@@ -36,7 +49,37 @@ int Menu::AddActioId(int TempActionId)
     // operation sucessfull so return true
 }
 
-void CreateMainMenu()
+void Menu::DisplayMenuElement()
 {
-    
+    BasicShapes ShapesObjects;
+
+    GLfloat Placeholder[] = {x,y, x+w,y, x+w,y-h, x,y-h};
+
+    glColor3f(1,0,0);
+    ShapesObjects.DrawQuad(Placeholder);
+
+    glColor3f(0,0,1);
+    DrawBitmapText(Name, x+(w/10), y-(h/2));
+
+}
+
+void DisplayMenu(Menu *MenuObject)
+{
+    for(auto MenuElement : MenuObject)
+    {
+        MenuElement.DisplayMenuElement()
+    }
+}
+
+void CreateMenu(Menu *MenuObject, char **MenuElementList, int ActionList, GLfloat tx, GLfloat ty, GLfloat Height, GLfloat Width)
+{
+    int i = 0;
+    GLfloat tempX = tx, tempY = ty;
+    for (auto MenuElement : MenuElementList)
+    {
+        MenuObject[i].AddName(MenuElement);
+        MenuObject[i].AddCoordinates(tempX, tempY, Height, Width);
+        MenuObject[i].AddActioId(ActionList[i]);
+        tempY = tempY - Height;
+    }
 }
