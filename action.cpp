21f0c183293f3f7fb625c9e3fd55bb62bf77eda6
Amanskywalker <ak47su30ac@gmail.com>
2017-05-09 15:30:10 +0530

    Added action functions 	new file:   action.cpp 	new file:   action.h

---
+++ action.cpp
@@ -0,0 +1,16 @@
+// header for the action functions
+
+#include "action.h"
+
+void MenuAction(int ActionId)
+{
+    switch (ActionId)
+    {
+        case 0:break;
+        case 1:break;
+        case 2:break;
+        case 3:break;
+        case 4: exit(0);break;
+        default:break;
+    }
+}
---
+++ action.h
@@ -0,0 +1,9 @@
+// header for the mouse functions
+#ifndef ACTION_H
+#define ACTION_H
+
+#include <stdlib.h>
+
+void MenuAction(int ActionId);
+
+#endif
