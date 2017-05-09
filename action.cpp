00dd21d2f728fabbf512410b21bae08ced2ac020
Amanskywalker <ak47su30ac@gmail.com>
2017-05-09 17:10:45 +0530

    Added action function array 	modified:   action.cpp 	modified:   shapes.h

--- action.cpp
+++ action.cpp
@@ -1,4 +1,4 @@
-// header for the action functions
+// definations action functions
 
 #include "action.h"
 
@@ -6,11 +6,11 @@ void MenuAction(int ActionId)
 {
     switch (ActionId)
     {
-        case 0:break;
         case 1:break;
         case 2:break;
         case 3:break;
-        case 4: exit(0);break;
+        case 4:break;
+        case 5: exit(0);break;
         default:break;
     }
 }
