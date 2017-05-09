4c91bae9a195a3b606c0f5be78094bd096731199
Amanskywalker <ak47su30ac@gmail.com>
2017-05-09 15:55:35 +0530

    Added Mouse functions 	new file:   mouse.cpp 	new file:   mouse.h

---
+++ mouse.cpp
@@ -0,0 +1,7 @@
+// definations mouse functions
+
+void MouseInputFuntion();
+void MenuAction(/* arguments */)
+{
+    /* code */
+}
---
+++ mouse.h
@@ -0,0 +1,9 @@
+// header for the mouse functions
+#ifndef MOUSE_H
+#define MOUSE_H
+
+void MenuAction(/* arguments */) {
+    /* code */
+}
+
+#endif
